299ec277fa079607defe66ca066930871e3716ae
Ruben <xinatowner@gmail.com>
2021-11-15 21:05:41 +0100

    Create domain delegation: test.beescloud.com

---
+++ domain-delegations/domain-delegation.tf
@@ -0,0 +1,10 @@
+# OPS-REQ - {{ values.subdomain }}
+
+a
+
+module "${values.subdomain}-beescloud-com" {
+  source   = "../modules/gcp-zone"
+  zone_id  = data.aws_route53_zone.beescloud_com.zone_id
+  hostname = "${values.subdomain}."
+  ns       = "${values.gcp-zone}"
+}
