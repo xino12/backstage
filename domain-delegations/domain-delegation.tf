5ed3a7a5041429a7a545220744dc7df10cbe179e
Ruben <xinatowner@gmail.com>
2022-03-09 12:11:44 +0100

    Create domain delegation: test.baguette.cloudbees.com

--- domain-delegations/domain-delegation.tf
+++ domain-delegations/domain-delegation.tf
@@ -1,10 +1,10 @@
 # OPS-REQ - {{ values.subdomain }}
-test
-a
+test.baguette.cloudbees.com
+c
 
-module "test-beescloud-com" {
+module "test.baguette.cloudbees.com-beescloud-com" {
   source   = "../modules/gcp-zone"
   zone_id  = data.aws_route53_zone.beescloud_com.zone_id
-  hostname = "test."
-  ns       = "a"
+  hostname = "test.baguette.cloudbees.com."
+  ns       = "c"
 }
